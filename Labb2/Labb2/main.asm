c26e8d933e7223b129767c0964bea9ea7e00f592
Skolmen <43547911+Skolmen@users.noreply.github.com>
2022-04-20 21:39:35 +0200

    Labb 2
    Snart klar. Kvar är att skicka ut ljud. Samt hantera okända tecken

--- Labb2/Labb2/main.asm
+++ Labb2/Labb2/main.asm
@@ -5,67 +5,96 @@ SETUP:
 	ldi		r16, LOW(RAMEND)
 	out		SPL, r16
 
-//---- Initierar saker ----
-INIT:
-	.equ	T=10
+	.equ	SHORT = 50
+	.equ	LONG = SHORT * 3
+	.equ	SPACE_WORDS = SHORT * 7
 	ser		r16
 	out		DDRB, r16
-	ldi		ZH, HIGH(MESSAGE*2)	; Ställer 
-	ldi		ZL, LOW(MESSAGE*2)
 	clr		r16
 
-MAIN_LOOP:
-	call GET_CHAR
-	rjmp MAIN_LOOP
+//---- Ställer in Z - pekaren ----
+START:
+	ldi		ZH, HIGH(MESSAGE * 2)	; Ställer stack pekaren för textmeddelandet
+	ldi		ZL, LOW (MESSAGE * 2)
 
+MORSE:
+	call	GET_CHAR
+	breq	START					; Om texten har körts återställs Z-pekaren
+	subi	r16, $41
+	brmi	IS_SPACE				; Om flaggan N är satt är tecknet ett minus
+	call	LOOKUP					; Ändrar r16 till binärkodat
+	call	SEND_CHAR				; Sänd ut karaktären på r16
+	rjmp	MORSE
+IS_SPACE:
+	call	SEND_SPACE
+	rjmp	MORSE
 
-
+//---- Hämtar karaktär från textmedelande ----------
 GET_CHAR:
-	push	r16					; Sparar undan r16
-NEXT_CHAR:
 	lpm		r16, Z+
 	cpi		r16, $00
-	breq	ALL_CHARS_FETCHED
-	call	SEND_CHAR
-	rjmp	NEXT_CHAR
-ALL_CHARS_FETCHED:
-	pop		r16					; Skickar tillbaka r16
 	ret
 
+//---- Översätter ASCII-tecknet till binärkodat ----
+LOOKUP:
+	push	ZH 
+	push	ZL
+	ldi		ZH, HIGH(BINARY_TABLE * 2)
+	ldi		ZL, LOW (BINARY_TABLE * 2)
+	add		ZL, r16
+	lpm		r16, Z
+	pop		ZL
+	pop		ZH
+	ret
+
+//---- Sänder ut karaktären ------------------------
 SEND_CHAR:
+	cpi		r16, $80					; Om r16 har värdet $80 har tecknet skickats
+	breq	DONE_WITH_CHAR
+	lsl		r16
+	brcc	SHORT_BEEP					; Om carry = 0 skicka en kort
+	brcs	LONG_BEEP					; Om carry = 1 skicka en lång
+SHORT_BEEP:
+	; Skicka en kort beep
+	rjmp	SEND_CHAR
+LONG_BEEP:
+	; Skicka en kort beep
+	rjmp	SEND_CHAR
+DONE_WITH_CHAR:
+	call	WAIT		; Vänta 3 beep
+	ret
+
+//---- Sänder ut mellanslag ------------------------
+SEND_SPACE:
+	call	WAIT		; Vänta 7 beep
+	ret
+
+WAIT:
+	
+	; Vänta antalet som behövs
 	
 	ret
 
+SOUND:
+	
+	; Skicka ut en signal på PORTB
 
-
-
-LONG_BEEP:
-	ldi		r16, T
-	push	r16
-	call	BEEP
 	ret
 
-SHORT_BEEP:
-	ldi		r16, T*2
-	push	r16
-	call	BEEP
+//Vet inte hur delayen riktigt ska fungera ännu
+DELAY:
+	ldi		r20, $1F
+DELAY_INNER_LOOP:
+	dec		r20
+	brne	DELAY_INNER_LOOP
+	dec		r21
+	brne	DELAY
 	ret
-
-BEEP:
-	out		PORTB, r18
-	ret
-
-
-
 
 
 //---- Textmedelande ----
 MESSAGE:
-	.db "DATORTEKNIK", $00
-
+	.db "TEKNIK", $00
+//---- Binary table ----
 BINARY_TABLE:
 	.db $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
-
-
-
-
